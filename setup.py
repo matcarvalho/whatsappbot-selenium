c722ad970b2c0dd6260448373ae42cea2a4d4695
Mateus Carvalho <mateuscarvalho@outlook.com>
2021-06-11 12:37:34 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 10 13:12:48 2021
+
+@author: mateuscarvalho
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='whatsappbot-selenium',
+    version='0.1.1',
+    url='https://github.com/matcarvalho/whatsappbot-selenium',
+    license='MIT License',
+    author='Mateus Carvalho',
+    author_email='mateuscarvalho@outlook.com',
+    keywords='Bot for Whatsapp with Selenium',
+    description=u'It is a module that has the function of sending and deleting whatsapp messages using Selenium',
+    packages=find_packages(),
+    install_requires=[],
+    test_suite='test',
+)
